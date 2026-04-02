837c74aac1e622774f24827278b9039828a11068
jangra-coder <codemasterjangra@gmail.com>
2026-04-02 15:02:46 +0530

    react_components

---
+++ reactapp/src/compoonents/Article.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Article() {
+return (
+    <div>
+        <h1>Article</h1>
+        <p>This is an article about React components.</p>
+        
+    </div>
+)
+}
+
+export default Article
